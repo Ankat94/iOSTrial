8d0cbc7d38f0f6d6d556228ea372a92d4d595022
Manish Ankat <ankat.manish@gmail.com>
2019-06-28 18:29:41 +0530

    Initial Commit

---
+++ iOSTrial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSTrial
+//
+//  Created by John Joshy DS on 28/06/19.
+//  Copyright © 2019 SRIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
